a189f9aec5cb67e8c0b2981badc6f0e9f7802385
ysedletskaya <ysedletskaya@gmail.com>
2016-07-29 18:20:58 +0300

    Intermediate (under construction) rotation logic

--- SeaFight.Core/SimpleShip.cs
+++ SeaFight.Core/SimpleShip.cs
@@ -27,7 +27,59 @@ namespace SeaFight.Core
 
         public void Rotate(Rotation rotation)
         {
-            //TODO: Create rotation logic
+            int deltaX;
+            int deltaY;
+            Deck deckHead = Decks.First();
+            if (IsShipRotatable())
+            {
+                GetRotationDirection(out deltaY, out deltaX);
+                switch (rotation)
+                {
+                    case Rotation.Left:
+                        for (int i = 1; i < Decks.Count - 1; i++)
+                        {
+                            Decks.ElementAt(i).X = deckHead.X - deltaX * i;
+                            Decks.ElementAt(i).Y = deckHead.Y - deltaY * i;
+                        }
+                        break;
+                    case Rotation.Right:
+                        for (int i = 1; i < Decks.Count - 1; i++)
+                        {
+                            Decks.ElementAt(i).X = deckHead.X + deltaX * i;
+                            Decks.ElementAt(i).Y = deckHead.Y + deltaY * i;
+                        }
+                        break;
+                }
+            }
+        }
+
+        private bool IsDeckRotatable(Deck deck, Rotation rotation, int detlaX, int deltaY)
+        {
+            switch (rotation)
+            {
+                case Rotation.Left:
+
+                    break;
+                case Rotation.Right:
+
+                    break;
+            }
+            return false;
+        }
+
+        private bool IsShipRotatable()
+        {
+            if (Decks.Count > 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void GetRotationDirection(out int xAxis, out int yAxis)
+        {
+            xAxis = Decks.ElementAt(0).X - Decks.ElementAt(1).X;
+            yAxis = Decks.ElementAt(0).X - Decks.ElementAt(1).X;
         }
 
         public void MoveShipToPoint(int x, int y)
